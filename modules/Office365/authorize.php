4def564ffa9c341ce2d84523ac2af62097c5756b
Cloud User <centos@ip-172-31-37-163.ap-southeast-1.compute.internal>
2018-05-18 17:30:01 +0800

    Global Changes

--- modules/Office365/authorize.php
+++ modules/Office365/authorize.php
@@ -2,8 +2,8 @@
 /**********************
 to fetch data office365
 **********************/
-
-	$redirect_uri="http://localhost/officeintegration/index.php?module=Office365&view=Authenticate&service=Office365";
+	global $site_URL;
+	$redirect_uri=$site_URL."/index.php?module=Office365&view=Authenticate&service=Office365";
 	session_start();
 	$_SESSION['code']=$_GET['code'];
     //echo "<pre>"; print_r($redirect_uri); die;
