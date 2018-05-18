c18975044e77b3e516c52645379e1088024be2cd
nirbhhayshah <nirbhay@secondcrm.com>
2018-05-18 18:32:57 +0800

    Base Structure for HR_Matters

--- sql_statements.sql
+++ sql_statements.sql
@@ -1,4 +1,7 @@
 
+
+/*********Added By Nirbhay For HR Matters ****************************/
+
 /*** Add a block in vtiger_settings_block HR_Matters with id 14*/
 
 /** Add the sub tabs *****************/
@@ -7,3 +10,7 @@ INSERT INTO ss_foundation.vtiger_settings_field (fieldid, blockid, name, iconpat
 INSERT INTO ss_foundation.vtiger_settings_field (fieldid, blockid, name, iconpath, description, linkto, sequence, active, pinned) VALUES (49, 14, 'LBL_CLAIM_TYPE', null, 'Claim Type', 'index.php?module=Vtiger&parent=Settings&view=ClaimTypeListView', 2, 0, 0);
 INSERT INTO ss_foundation.vtiger_settings_field (fieldid, blockid, name, iconpath, description, linkto, sequence, active, pinned) VALUES (50, 14, 'LBL_BENEFIT_TYPE', null, 'Benifit Type', 'index.php?module=Vtiger&parent=Settings&view=BenefitTypeListView', 3, 0, 0);
 INSERT INTO ss_foundation.vtiger_settings_field (fieldid, blockid, name, iconpath, description, linkto, sequence, active, pinned) VALUES (51, 14, 'LBL_ALLOCATION', null, 'Allocation', 'index.php?module=Vtiger&parent=Settings&view=AllocationListView', 4, 0, 0);
+
+
+
+/************** HR Matters End ***************************************/
