cc555d7c43da02a8086486ed619e7568339d87e2
Jitu <jitu@secondcrm.com>
2018-06-01 19:04:39 +0800

    Parent Tab

--- parent_tabdata.php
+++ parent_tabdata.php
@@ -6,7 +6,7 @@
 $parent_tab_info_array=array(9=>'Foundation',10=>'Admin',7=>'Tools');
 
 
-$parent_child_tab_rel_array=array(9=>array(64,),10=>array(63,62,),7=>array(31,31,40,40,46,46,40,46,),);
+$parent_child_tab_rel_array=array(9=>array(64,),10=>array(63,62,69,),7=>array(31,31,40,40,46,46,40,46,),);
 
 
 
