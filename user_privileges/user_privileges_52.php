0998f026ff68b954341feb05087c2505f7a87f06
Cloud User <centos@ip-172-31-37-163.ap-southeast-1.compute.internal>
2018-06-11 12:51:12 +0800

    User pric

---
+++ user_privileges/user_privileges_52.php
@@ -0,0 +1,28 @@
+<?php
+
+
+//This is the access privilege file
+$is_admin=false;
+
+$current_user_roles='H4';
+
+$current_user_parent_role_seq='H1::H2::H3::H4';
+
+$current_user_profiles=array(2,);
+
+$profileGlobalPermission=array('1'=>1,'2'=>1,);
+
+$profileTabsPermission=array('1'=>1,'2'=>0,'3'=>0,'4'=>0,'6'=>0,'7'=>0,'8'=>1,'9'=>1,'10'=>1,'13'=>1,'14'=>0,'15'=>1,'16'=>1,'18'=>0,'19'=>0,'20'=>0,'21'=>0,'22'=>0,'23'=>0,'24'=>1,'25'=>0,'26'=>0,'27'=>1,'30'=>0,'31'=>1,'32'=>0,'33'=>0,'34'=>0,'35'=>1,'36'=>0,'37'=>0,'38'=>1,'39'=>0,'40'=>0,'41'=>1,'42'=>1,'43'=>1,'44'=>1,'45'=>1,'46'=>0,'47'=>1,'48'=>1,'49'=>0,'50'=>1,'51'=>0,'53'=>0,'54'=>0,'55'=>0,'56'=>0,'57'=>0,'58'=>0,'59'=>0,'60'=>0,'62'=>0,'63'=>0,'64'=>0,'65'=>0,'66'=>0,'67'=>0,'28'=>1,);
+
+$profileActionPermission=array(2=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,10=>0,),4=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,8=>0,10=>0,),6=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,8=>0,10=>0,),7=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,8=>0,9=>0,10=>0,),8=>array(0=>0,1=>0,2=>0,4=>0,7=>0,6=>0,),14=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,10=>0,),18=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,10=>0,),19=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,10=>0,),20=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,),21=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,),22=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,),23=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,),25=>array(0=>1,1=>0,2=>0,4=>0,7=>0,6=>0,13=>0,),26=>array(0=>0,1=>0,2=>0,4=>0,7=>0,),34=>array(0=>0,1=>0,2=>0,4=>0,7=>0,11=>0,12=>0,),36=>array(0=>0,1=>0,2=>0,4=>0,7=>0,5=>0,6=>0,10=>0,),51=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),53=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),54=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),55=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),56=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),57=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),58=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),59=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),60=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),62=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),63=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),64=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),65=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),66=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,5=>0,6=>0,8=>0,),67=>array(0=>0,1=>0,2=>0,3=>0,4=>0,7=>0,),9=>array(5=>0,6=>0,),13=>array(5=>0,6=>0,8=>0,10=>0,),35=>array(5=>0,6=>0,10=>0,),43=>array(5=>0,6=>0,10=>0,),44=>array(5=>0,6=>0,10=>0,),45=>array(5=>0,6=>0,10=>0,),);
+
+$current_user_groups=array(2,3,4,);
+
+$subordinate_roles=array('H5','H7',);
+
+$parent_roles=array('H1','H2','H3',);
+
+$subordinate_roles_users=array('H5'=>array(8,),'H7'=>array(5,),);
+
+$user_info=array('user_name'=>'rahimah','is_admin'=>'','user_password'=>'$1$ra000000$9PWtHE6tBjNErLO6bHZVK1','confirm_password'=>'$1$ra000000$9PWtHE6tBjNErLO6bHZVK1','first_name'=>'Rahimah','last_name'=>'SS','roleid'=>'H4','email1'=>'norahimah@secondcrm.com','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'09:00','is_owner'=>'','title'=>'Manager','phone_work'=>'03-86874433','department'=>'CS','phone_mobile'=>'012-9737427','reports_to_id'=>'1','phone_other'=>'0300000000','email2'=>'norahimah.s@gmail.com','phone_fax'=>'0399999999','secondaryemail'=>'rahimahrs007@gmail.com','phone_home'=>'0300000000','date_format'=>'yyyy-mm-dd','signature'=>'&lt;span style=&quot;color:#B22222;&quot;&gt;&lt;span style=&quot;font-family:arial, helvetica, sans-serif;&quot;&gt;Thanks &amp; Best Regards,&lt;/span&gt;&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;color:#800080;&quot;&gt;&lt;span style=&quot;font-family:arial, helvetica, sans-serif;&quot;&gt;&lt;strong&gt;Rahimah&lt;/strong&gt;&lt;/span&gt;&lt;/span&gt;&lt;br /&gt;&lt;br /&gt;&lt;span style=&quot;color:#0000CD;&quot;&gt;&lt;span style=&quot;font-family:arial, helvetica, sans-serif;&quot;&gt;Agiliux Product Manager&lt;/span&gt;&lt;/span&gt;','description'=>'No docs','address_street'=>'C2-3-7, CBD 3','address_city'=>'Cyberjaya','address_state'=>'Selangor','address_postalcode'=>'63000','address_country'=>'Malaysia','accesskey'=>'iG0ZCvUW3ZYyMC','time_zone'=>'UTC','currency_id'=>'6','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'en_us','reminder_interval'=>'','phone_crm_extension'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Sunday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'0','rowheight'=>'medium','defaulteventstatus'=>'','defaultactivitytype'=>'','hidecompletedevents'=>'0','defaultcalendarview'=>'','currency_name'=>'Malaysia, Ringgits','currency_code'=>'MYR','currency_symbol'=>'RM','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'52');
+?>
