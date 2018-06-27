305af2347b7e5a2566571906df4f3cf88c3c1cae
Jitu <jitu@secondcrm.com>
2018-06-27 11:25:32 +0800

    Bug in login history

--- layouts/v7/modules/Settings/LoginHistory/resources/List.js
+++ layouts/v7/modules/Settings/LoginHistory/resources/List.js
@@ -187,11 +187,7 @@ Settings_Vtiger_List_Js("Settings_LoginHistory_List_Js",{},{
 						app.hideModalWindow();
 					});
 					aDeferred.resolve(data);
-<<<<<<< HEAD
-
-=======
->>>>>>> 8d665187f3149b6b205f3cedc5573b695766f8ae
-					history.pushState({}, null, window.history.back());
+     				history.pushState({}, null, window.history.back());
 
 				},
 				function(error,err) { 
