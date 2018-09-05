ec38bbe44e0ab0479ba448d9ad91c300c950f5c0
root <root@localhost.localdomain>
2018-09-05 18:20:01 +0800

    Action Feature for HRAdmin

--- modules/Settings/Performance/models/Record.php
+++ modules/Settings/Performance/models/Record.php
@@ -27,7 +27,7 @@ class Settings_Performance_Record_Model extends Settings_Vtiger_Record_Model {
 	public function getRecordLinks() {
 		$currentuser = Users_Record_Model::getCurrentUserModel();
 		$links = array();
-		if($currentuser->get('is_admin')=='on'){
+		if($currentuser->get('is_admin')=='on' || $currentuser->get('hradmin')=='1'){
 			$recordLinks = array(
 				array(
 					'linktype' => 'LISTVIEWRECORD',
