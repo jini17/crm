b7868eef1fd973a287d0a7b51ef0cb66d3ae9b99
Cloud User <centos@ip-172-31-37-163.ap-southeast-1.compute.internal>
2018-09-25 17:53:02 +0800

    Custom module

--- addUpdateInModule.php
+++ addUpdateInModule.php
@@ -4,7 +4,7 @@
 include_once('vtlib/Vtiger/Module.php');
 require_once('modules/ModTracker/ModTracker.php');
 $Vtiger_Utils_Log = true;
-$module = Vtiger_Module::getInstance('MessageBoard');
+$module = Vtiger_Module::getInstance('Training');
 ModTracker::disableTrackingForModule($module->id);
 ModTracker::enableTrackingForModule($module->id);
 
--- parent_tabdata.php
+++ parent_tabdata.php
@@ -4,8 +4,9 @@
 //This file contains the commonly used variables 
 
 $parent_tab_info_array=array(9=>'Foundation',10=>'Admin',3=>'Sales',7=>'Tools',4=>'Support');
-$parent_child_tab_rel_array=array(9=>array(64,74,75,),10=>array(63,62,69,71,73,),3=>array(7,6,4,2,20,22,23,19,8,9,47,47,54,65,66,72,),7=>array(31,31,40,40,46,46,40,46,),4=>array(13,15,6,4,8,28,9,35,35,43,43,44,44,45,45,51,53,55,56,57,58,60,67,),);
 
+
+$parent_child_tab_rel_array=array(9=>array(64,73,74,),10=>array(63,62,69,71,),3=>array(4,8,9,47,47,54,65,66,72,),7=>array(31,31,40,40,46,46,40,46,),4=>array(4,8,28,9,35,35,51,53,55,56,57,58,60,67,),);
 
 
 
