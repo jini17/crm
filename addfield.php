9599005af918411c065546e8b4e154f518b9be15
Jitu <jitu@secondcrm.com>
2019-01-02 10:09:31 +0800

    Addfield

--- addfield.php
+++ addfield.php
@@ -12,35 +12,23 @@ global $adb;
 $adb->setDebug(true);
 
 $module = new Vtiger_Module();
-$module->name = 'Users';
-$module = $module->getInstance('Users');
+$module->name = 'OrganizationDetails';
+$module = $module->getInstance('OrganizationDetails');
 
 // Create Block instance
 
 $block = new Vtiger_Block();
-$block->label = 'LBL_EMPLOYEE_LOCATION_INFORMATION';
-$module->addBlock($block);
+$block->label = 'LBL_ORGANIZATIONDETAILS_INFORMATION';
+$block = $block->getInstance($block->label,$module);
 
 
 $field1 = new Vtiger_Field();
-$field1->name = 'user_company';
-$field1->label = 'Select Company';
+$field1->name = 'isdefault';
+$field1->label = 'Default Company';
 $field1->table = $module->basetable;
-$field1->column = 'user_company';
-$field1->columntype = 'varchar(10)';
-$field1->uitype = 3994;
-$field1->displaytype = 1;
-$field1->typeofdata = 'V~O'; // varchar~Mandatory
-$block->addField($field1); /** table and column are automatically set */
-
-
-$field1 = new Vtiger_Field();
-$field1->name = 'user_location';
-$field1->label = 'Select Location';
-$field1->table = $module->basetable;
-$field1->column = 'user_location';
-$field1->columntype = 'varchar(10)';
-$field1->uitype = 3996;
+$field1->column = 'isdefault';
+$field1->columntype = 'varchar(1)';
+$field1->uitype = 56;
 $field1->displaytype = 1;
 $field1->typeofdata = 'V~O'; // varchar~Mandatory
 $block->addField($field1); /** table and column are automatically set */
