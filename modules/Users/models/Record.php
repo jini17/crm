f12140b97f578a2d61f67bcf75aae46ee8f801d4
Khaled Mahmud <khaled@softsolvers.my.com>
2019-02-12 00:44:13 +0800

    delta fix

--- modules/Users/models/Record.php
+++ modules/Users/models/Record.php
@@ -8,17 +8,7 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  *************************************************************************************/
-error_reporting(1);
-        ini_set('display_erros',1);         
-          register_shutdown_function('handleErrors');       
-            function handleErrors() { 
-               $last_error = error_get_last(); 
-               if (!is_null($last_error)) { 
-               // if there has been an error at some point 
-               // do something with the error 
-               print_r($last_error); 
-               } 
-            }
+
 
 class Users_Record_Model extends Vtiger_Record_Model
 {
