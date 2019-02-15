872ac7879c97da22eb3cc7fae4cc9437f6c0744d
root <root@ip-172-31-37-163.ap-southeast-1.compute.internal>
2019-02-15 20:27:01 +0800

    Lang pack

---
+++ languages/th_th/Accounts.php
@@ -0,0 +1,64 @@
+<?php
+/*+***********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ *************************************************************************************/
+$languageStrings = array(
+	// Basic Strings
+	'Accounts' => 'Tài khoản',
+	'SINGLE_Accounts' => 'Tài khoản',
+	'LBL_ADD_RECORD' => 'Thêm vào Tài khoản',
+	'LBL_RECORDS_LIST' => 'Tài khoản List',
+
+	// Blocks
+	'LBL_ACCOUNT_INFORMATION' => 'Tài khoản Details',
+
+	// Mass Action
+	'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Tài khoản Hierarchy',
+
+	//Field Labels
+	'industry' => 'Industry',
+	'Account Name' => 'Tài khoản Name',
+	'Account No' => 'Tài khoản Number',
+	'Website' => 'Website',
+	'Ticker Symbol' => 'Ticker Symbol',
+	'Member Of' => 'Member Of',
+	'Employees' => 'Employees',
+	'Ownership' => 'Ownership',
+	'SIC Code' => 'Company Registration No',
+	'Other Email' => 'Other Email',
+	'Other Phone' => 'Other Phone',
+	'Phone' => 'Phone',
+	'Email' => 'Email',
+	'Type' => 'Type',
+
+	//Added for existing picklist entries
+	'Analyst'=>'Analyst',
+	'Competitor'=>'Competitor',
+	'Customer'=>'Contacts',
+	'Integrator'=>'Integrator',
+	'Investor'=>'Investor',
+	'Press'=>'Press',
+	'Prospect'=>'Prospect',
+	'Reseller'=>'Reseller',
+	'LBL_START_DATE' => 'Start Date',
+	'LBL_END_DATE' => 'End Date',
+
+	//Duplication error message
+	'LBL_DUPLICATES_EXIST' => 'Account Name already exists',
+	'LBL_COPY_SHIPPING_ADDRESS' => 'Copy Shipping Address',
+	'LBL_COPY_BILLING_ADDRESS' => 'Copy Billing Address',
+	'LBL_IMAGE_INFORMATION' => 'Profile Picture',
+	'Organization Image' => 'Account Image',
+);
+
+$jsLanguageStrings = array(
+	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to delete?',
+	'LBL_DELETE_CONFIRMATION' => 'Deleting this Account will remove its related Opportunities & Quotes. Are you sure you want to delete this Account?',
+	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this Account(s) will remove its related Opportunities & Quotes. Are you sure you want to delete the selected records?',
+	'JS_DUPLICATE_CREATION_CONFIRMATION' => 'Account Name already Exists.Do you want to create a duplicate record?'
+);
---
+++ languages/th_th/custom/Users.php
@@ -0,0 +1,8 @@
+<?php
+$languageStrings = array(
+'Human Resource & Management'	=>	'Human Resource & Management',
+'Technical'	=>	'Technical',
+'Support'	=>	'Support',
+'Marketing'	=>	'Marketing',
+'Administrator'	=>	'Administrator',
+);
